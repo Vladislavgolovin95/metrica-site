c8af6d3f7fd9cd91c794979ef4aaba8129110ed5
Vladislav Golovin <vladislav.golovin95@mail.ru>
2023-04-13 23:23:22 +0300

    add icon burger menu

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 const POPUP_OPENED_CLASSNAME = 'popup_open';
-const BODY_FIXED_CLASSNAME = 'body_fixed';
+const BODY_FIXED_CLASSNAME_2 = 'body_fixed';
 
-const bodyNode = document.querySelector('body');
+const bodyNode2 = document.querySelector('body');
 const popupNode = document.querySelector('.js-popup');
 const btnOpenNode = document.querySelector('.js-btn');
 const popupContentNode = document.querySelector('.js-popup__content')
@@ -20,5 +20,5 @@ popupNode.addEventListener('click', (event) => {
 
 function togglePopup() {
     popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
-    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+    bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME_2);
 }
